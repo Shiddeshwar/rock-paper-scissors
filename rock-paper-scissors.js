0af5e868441a55da4a9cf81254fdddcd1a4334c5
Shiddeshwar <49190598+Shiddeshwar@users.noreply.github.com>
2021-04-18 15:55:50 +0530

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,13 @@
 function game() 
 {
     let player = 0, computer = 0;
+    //computerPlay() returns rock/paper/scissors randomly
     function computerPlay() {
         let options = ["rock","paper","scissors"]; 
         let rand = Math.floor(Math.random()*3);
         return options[rand];
     }
+    //playRound() Compares the values provided by Player/Human and computer(computerPlay()).
     function playRound(playerSelection, computerSelection) 
     {
         if (computerSelection === playerSelection) 
@@ -31,12 +33,13 @@ function game()
     }
     for (let i = 0; i < 5; i++) 
     {
-        const playerSelection = prompt("Enter rock/paper/scissors:","scissors").toLowerCase();
+        const playerSelection = prompt("Enter rock/paper/scissors:","scissors").toLowerCase(); //Value Provided by human
         const computerSelection = computerPlay();
         let res = playRound(playerSelection, computerSelection);
         console.log(res);
         console.log("Score is Human: " + player + " & Computer: " + computer);
     }
+    //Code below is to decide the final winner.
     if(player === computer) console.log("Its a TIE!");
     else {
         let winner = player>computer?"Human is the winner!":"Computer is the winner!";
