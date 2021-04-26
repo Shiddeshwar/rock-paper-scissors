f5330a61fa73b5dc735b0e7cad3031a01597b2e1
Shiddeshwar <49190598+Shiddeshwar@users.noreply.github.com>
2021-04-26 07:01:04 +0530

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,23 +6,10 @@ function computerPlay() {
 }
 function playRound(playerSelection, computerSelection) 
 {
-<<<<<<< HEAD
     if (computerSelection === playerSelection) 
-=======
-    let player = 0, computer = 0;
-    //computerPlay() returns rock/paper/scissors randomly
-    function computerPlay() {
-        let options = ["rock","paper","scissors"]; 
-        let rand = Math.floor(Math.random()*3);
-        return options[rand];
-    }
-    //playRound() Compares the values provided by Player/Human and computer(computerPlay()).
-    function playRound(playerSelection, computerSelection) 
->>>>>>> 0af5e868441a55da4a9cf81254fdddcd1a4334c5
     {
         return computerSelection + " & " + playerSelection+"! So it's a TIE!";
     }
-<<<<<<< HEAD
     else if (computerSelection === "rock" && playerSelection === "scissors") {
         computer +=1;
             return "Rock beats Scissors! Computer Wins!";
@@ -35,18 +22,6 @@ function playRound(playerSelection, computerSelection)
         computer +=1;
         return "Scissors beats Paper! Computer Wins!";
     }
-=======
-    for (let i = 0; i < 5; i++) 
-    {
-        const playerSelection = prompt("Enter rock/paper/scissors:","scissors").toLowerCase(); //Value Provided by human
-        const computerSelection = computerPlay();
-        let res = playRound(playerSelection, computerSelection);
-        console.log(res);
-        console.log("Score is Human: " + player + " & Computer: " + computer);
-    }
-    //Code below is to decide the final winner.
-    if(player === computer) console.log("Its a TIE!");
->>>>>>> 0af5e868441a55da4a9cf81254fdddcd1a4334c5
     else {
         player += 1;
         return playerSelection + " beats " + computerSelection + "! " + "Human Wins!";
@@ -63,6 +38,7 @@ function game(select) {
     comm.innerHTML = res;
     document.getElementById('round').innerHTML = "Round " + gameCount;
     if (player>= 5 || computer >= 5) {
+
         let winner = player>computer?"Human is the winner!":"Computer is the winner!";
         comm.innerHTML = winner;
         gameCount = 0, player = 0, computer = 0;
@@ -83,8 +59,4 @@ function clickHandler(event) {
         game(playerSelect);
     }
 }
-<<<<<<< HEAD
 document.body.addEventListener("click", clickHandler);
-=======
-game();
->>>>>>> 0af5e868441a55da4a9cf81254fdddcd1a4334c5
