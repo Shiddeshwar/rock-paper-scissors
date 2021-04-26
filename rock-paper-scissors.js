57f736dc94bda34ec1362b08d59e9abc767325a7
Shiddeshwar <siddeshwarnadar@gmail.com>
2021-04-26 06:34:03 +0530

    Added Some UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,46 +1,61 @@
-function game() 
+let player = 0, computer = 0, gameCount = 0;
+function computerPlay() {
+    let options = ["rock","paper","scissors"]; 
+    let rand = Math.floor(Math.random()*3);
+    return options[rand];
+}
+function playRound(playerSelection, computerSelection) 
 {
-    let player = 0, computer = 0;
-    function computerPlay() {
-        let options = ["rock","paper","scissors"]; 
-        let rand = Math.floor(Math.random()*3);
-        return options[rand];
-    }
-    function playRound(playerSelection, computerSelection) 
+    if (computerSelection === playerSelection) 
     {
-        if (computerSelection === playerSelection) 
-        {
-            return computerSelection + " & " + playerSelection+"! So it's a TIE!";
-        }
-        else if (computerSelection === "rock" && playerSelection === "scissors") {
-            computer +=1;
-             return "Rock beats Scissors! Computer Wins!";
-        }
-        else if (computerSelection === "paper" && playerSelection === "rock") {
-            computer +=1;
-            return "Paper beats Rock! Computer Wins!";
-        }
-        else if (computerSelection === "scissors" && playerSelection === "paper") {
-            computer +=1;
-            return "Scissors beats Paper! Computer Wins!";
-        }
-        else {
-            player += 1;
-            return playerSelection + " beats " + computerSelection + "! " + "Human Wins!";
-        }
+        return computerSelection + " & " + playerSelection+"! So it's a TIE!";
     }
-    for (let i = 0; i < 5; i++) 
-    {
-        const playerSelection = prompt("Enter rock/paper/scissors:","scissors").toLowerCase();
-        const computerSelection = computerPlay();
-        let res = playRound(playerSelection, computerSelection);
-        console.log(res);
-        console.log("Score is Human: " + player + " & Computer: " + computer);
+    else if (computerSelection === "rock" && playerSelection === "scissors") {
+        computer +=1;
+            return "Rock beats Scissors! Computer Wins!";
     }
-    if(player === computer) console.log("Its a TIE!");
+    else if (computerSelection === "paper" && playerSelection === "rock") {
+        computer +=1;
+        return "Paper beats Rock! Computer Wins!";
+    }
+    else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computer +=1;
+        return "Scissors beats Paper! Computer Wins!";
+    }
     else {
-        let winner = player>computer?"Human is the winner!":"Computer is the winner!";
-        console.log(winner);
+        player += 1;
+        return playerSelection + " beats " + computerSelection + "! " + "Human Wins!";
     }
 }
-game();
+function game(select) {
+    gameCount += 1;
+    let playerSelection= select;
+    const computerSelection = computerPlay();
+    let res = playRound(playerSelection, computerSelection);
+    document.getElementById('human').innerHTML = player;
+    document.getElementById('computer').innerHTML = computer;
+    let comm = document.getElementById('Commentary');
+    comm.innerHTML = res;
+    document.getElementById('round').innerHTML = "Round " + gameCount;
+    if (player>= 5 || computer >= 5) {
+        let winner = player>computer?"Human is the winner!":"Computer is the winner!";
+        comm.innerHTML = winner;
+        gameCount = 0, player = 0, computer = 0;
+        document.body.removeEventListener('click',clickHandler);
+        let button = document.createElement('button');
+        button.innerHTML = "Play Again";
+        button.classList.add('keys','key');
+        document.body.insertBefore(button,document.body.lastElementChild);
+        button.addEventListener('click',playAgain);
+    }
+}
+function playAgain() {
+    location.reload();
+}
+function clickHandler(event) {
+    if (event.target.nodeName == "BUTTON") {
+        let playerSelect = event.target.id;
+        game(playerSelect);
+    }
+}
+document.body.addEventListener("click", clickHandler);
